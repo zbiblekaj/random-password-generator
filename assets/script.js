c92b47db5be7bdb2b7a31a09c4ac24a7b4913cd3
zbiblekaj <zanabiblekaj@gmail.com>
2023-03-02 12:16:16 +0100

    object-of-arrays-for-characters

--- assets/script.js
+++ assets/script.js
@@ -1,120 +1,77 @@
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
-  ' '
-];
+// an object conatining an aray of characters to be included in password: special, numeric, lowercase & uppercase.
+const characters = {
+  specialCharacters = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ' '];
+  numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+}
 
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Function for getting a random element from an array. rock paper scissors.
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+// an object containing functions containing all of the above characters.
+const getCharacters = {
+  function specialCharacters(){
+    return characters.specialCharacters[Math.floor(Math.random() * character.specialCharacters.length)];
+  }
+  function numericCharacters(){
+    return characters.numericCharacters[Math.floor(Math.random() * character.numericCharacters.length)];
+  }
+  function lowerCasedCharacters(){
+    return characters.lowerCasedCharacters[Math.floor(Math.random() * character.lowerCasedCharacters.length)];
+  }
+  function upperCasedCharacters(){
+    return characters.upperCasedCharacters[Math.floor(Math.random() * character.upperCasedCharacters.length)];
+  }
+}
 
 // define variables
 let password = "";
 let passwordLength = [];
 
-// Function to prompt user for password options. all of the prompts should be wrapped in this function.
+// function to prompt user for all password options.
 function userOptions() {
-  // password length.
-  let passwordLength = parseInt(prompt("Enter the length you would like your password to be - between 10 & 64 characters: ")); 
+  // create a prompt for the user to determine their password length.
+    // validate the input - ensure the character length is between 10 & 64.
+  let passwordLength = parseInt(prompt("How many characters would you like your password to have? - between 10 & 64 characters: ")); 
     if (passwordLength >= 10 && passwordLength <= 64) {
       alert ("You have selected ${passwordLength} characters");
-    } else {
+    } 
+    
+    
+    else {
       alert ("Response is not valid");
     }
+    // lowercase, upercase, or special characters
+      //validate the input = ensure at least one character type is being selected.
       let includeCaps = confirm ("Press OK if you would like to include capital letters in your password, otherwise, press cancel.");
       let includeLowerCase = confirm ("Press OK if you would like to include lower case letters in your password, otherwise, press cancel.");
       let includeNumbers = confirm ("Press OK if you would like to include numbers in your password, otherwise, press cancel.");
-    
+      let includeSpecialCharacters = confirm ("Press OK if you would like to include special characters in your password, otherwise, press cancel.");
+  
+  //display generated password on the page.
+  return "generated password";
 }
 
-// Function for getting a random element from an array. rock paper scissors.
-function getRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+
 
 // Function to generate password with user input. use above functions to implement this.
+
+let character = [];
+
 function generatePassword() {
+  userOptions();
+  if (userOptions.includeCaps) {
+    characters = characters.concat(upperCasedCharacters)
+  } if (userOptions.includeLowerCase) {
+    characters = characters.concat(lowerCasedCharacters)
+  } if (userOptions.includeNumbers) {
+    characters = characters.concat(numericCharacters) 
+  } if (userOptions.includeSpecialCharacters) {
+    characters = characters.concat(specialCharacters);
+  }
 }
 
 // Get references to the #generate element. 
